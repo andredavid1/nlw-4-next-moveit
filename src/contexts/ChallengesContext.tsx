0760c4f968b459e0e8bae70dba7fbe789852a78c
André David <andredavid1@yahoo.com.br>
2021-02-26 15:35:32 -0300

    feat: create notification web

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useState} from 'react'
+import { createContext, ReactNode, useEffect, useState} from 'react'
 import challenges from '../../challenges.json';
 
 interface Challenge {
@@ -31,6 +31,10 @@ export function ChallengesProvider ({ children }: ChallengesProviderProps) {
   const [activeChallenge, setActiveChallenge] = useState(null);
 
   const experienceToNextLevel = Math.pow((level + 1) * 4, 2)
+
+  useEffect(() => {
+    Notification.requestPermission();
+  }, []);
   
   function levelUp() {
     setLevel(level + 1);
@@ -41,6 +45,12 @@ export function ChallengesProvider ({ children }: ChallengesProviderProps) {
     const challenge = challenges[randomChallengeIndex];
 
     setActiveChallenge(challenge)
+
+    if (Notification.permission === 'granted') {
+      new Notification('Novo desafio', {
+        body: `Valendo ${challenge.amount}xp`
+      })
+    }
   }
 
   function resetChallenge() {
