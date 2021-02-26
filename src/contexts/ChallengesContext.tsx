7ea3f54e08f2f6fba7fca5c2549150e52111d73f
André David <andredavid1@yahoo.com.br>
2021-02-26 18:15:02 -0300

    refactor: correct variable value challengesCompleted

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -31,7 +31,7 @@ export const ChallengesContext = createContext({} as ChallengesContextData);
 export function ChallengesProvider ({ children, ...rest }: ChallengesProviderProps) {
   const [level, setLevel] = useState(rest.level ?? 1);
   const [currentExperience, setCurrentExperience] = useState(rest.currentExperience ?? 0);
-  const [challengesCompleted, setChallengeCompleted] = useState(rest.currentExperience ?? 0);
+  const [challengesCompleted, setChallengeCompleted] = useState(rest.challengesCompleted ?? 0);
   const [activeChallenge, setActiveChallenge] = useState(null);
 
   const experienceToNextLevel = Math.pow((level + 1) * 4, 2)
